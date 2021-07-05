72d8528e4d9062123afb22c2296e6702766e690c
Abuoblie <Abu.oblie@gmail.com>
2021-07-05 15:21:30 +0200

    first commit

---
+++ Robot.java
@@ -0,0 +1,14 @@
+/**
+ * Robot
+ */
+import java.util.*;
+public class Robot {
+
+        
+        /** 
+         * @param args
+         */
+        public static void main(String[] args) {
+                
+        }
+}
