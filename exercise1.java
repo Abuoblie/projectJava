b263bf63e6f0f2675d5c2582b63840b59fcfbdea
nataliacherman08 <cherman.natalia08@gmail.com>
2021-07-07 15:59:44 +0200

    Update exercise variables

---
+++ exercise1.java
@@ -0,0 +1,17 @@
+public class MyVariable {
+    public static void main(String[] args)    {
+     
+     //Déclarez la variable int salary
+     int salary = 1500;
+     //Déclarez la variable int savings
+     int savings = 3000;
+     
+     //TODO : Déclarez une variable int nommée bonusSalary avec la valeur 250
+    
+    
+     //Affiche le résultat
+     System.out.println("You have an ongoing salary of " + salary +"\nYour savings amount is " + savings +"\nYou have a bonus salary of " + bonusSalary);
+    
+    }
+     
+}
